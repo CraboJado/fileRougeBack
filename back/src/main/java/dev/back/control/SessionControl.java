c621a0409ae88088d8b4a0b5c53752c2685879d8
liting <liting_qiu@hotmail.com>
2023-09-12 21:59:55 +0200

    refactoring

--- back/src/main/java/dev/back/control/SessionControl.java
+++ back/src/main/java/dev/back/control/SessionControl.java
@@ -41,7 +41,7 @@ public class SessionControl {
         System.out.println(loginDTO.toString());
         return this.employeRepo.findByEmail(loginDTO.getEmail())
                 .filter( employe ->  passwordEncoder.matches(loginDTO.getPassword(), employe.getPassword()))
-                .map( employe -> ResponseEntity.ok().header(HttpHeaders.SET_COOKIE,buildJWTCookie(employe)).body(employe.getRoles()))
+                .map( employe -> ResponseEntity.ok().header(HttpHeaders.SET_COOKIE,buildJWTCookie(employe)).build())
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
     }
 
